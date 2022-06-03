d93063a915ee60dd7ec01d910b0aabd27f1c35b4
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-03 16:51:15 +0530

    [Biplab] Add: Person class

---
+++ LambdaProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //uc 1
+            List<Person> listPerson = new List<Person>();
+            listPerson.Add(new Person("10000", "Ram", "Assam", 50));
+            listPerson.Add(new Person("10001", "Shyam", "West Bengal", 60));
+            listPerson.Add(new Person("10002", "Jodu", "Bihar", 70));
+            listPerson.Add(new Person("10003", "Madhu", "UP", 55));
+            listPerson.Add(new Person("10004", "Bikash", "Andhra Pradesh", 15));
+            listPerson.Add(new Person("10005", "Bimal", "Telangana", 75));
+            listPerson.Add(new Person("10004", "Biki", "Maharastra", 18));
+
+            foreach (Person person in listPerson.ToList())
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+    }
+}
